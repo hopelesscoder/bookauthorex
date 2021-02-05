5584c9b31f41d6886d9eaac74ba486a2bf9793ff
hopelesscoder <dadda-1993@hotmail.it>
2021-02-05 23:47:12 +0100

    Annotation added

---
+++ src/main/java/com/example/bookauthor/Author.java
@@ -0,0 +1,67 @@
+package com.example.bookauthor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String address;
+    private int telephone;
+    private List<String> comments;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+}
--- src/main/java/com/example/bookauthor/Book.java
+++ src/main/java/com/example/bookauthor/Book.java
@@ -3,12 +3,11 @@
  */
 package com.example.bookauthor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author daniele pasquini
@@ -22,6 +21,14 @@ public class Book {
 	private int id;
 	private String title;
 	private int isbn;
+	private int pages;
+	private  Boolean published;
+	private String description;
+	private String language;
+	private int year;
+	@ElementCollection
+	private List<String> categories;
+	private List<Author> authors;
 
 	public int getId() {
 		return id;
@@ -44,5 +51,61 @@ public class Book {
 
 	public void setIsbn(int isbn) {
 		this.isbn = isbn;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public Boolean getPublished() {
+		return published;
+	}
+
+	public void setPublished(Boolean published) {
+		this.published = published;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public List<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<String> categories) {
+		this.categories = categories;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
 	}
 }
