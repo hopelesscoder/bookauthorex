9b385c833fb6e1333ffa8e17f4e0a00e2f372380
hopelesscoder <dadda-1993@hotmail.it>
2021-02-06 15:29:18 +0100

    status code added

--- src/main/java/com/example/bookauthor/Author.java
+++ src/main/java/com/example/bookauthor/Author.java
@@ -16,7 +16,7 @@ public class Author {
     private int telephone;
     @ElementCollection
     private List<String> comments;
-    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "author")
     //@JoinColumn(name="book_id", referencedColumnName="id")
     @JsonIgnoreProperties("author")
     List<Book> books;
