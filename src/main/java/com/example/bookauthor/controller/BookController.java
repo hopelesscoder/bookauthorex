5907bd4a98cc7390e8fd0e0e5ef7fe2b3a909562
hopelesscoder <dadda-1993@hotmail.it>
2021-02-14 14:28:05 +0100

    BugFix

--- src/main/java/com/example/bookauthor/controller/BookController.java
+++ src/main/java/com/example/bookauthor/controller/BookController.java
@@ -34,16 +34,14 @@ public class BookController {
 											   @RequestParam(required = false) Boolean published,
 											   @RequestParam(required = false) String description,
 											   @RequestParam(required = false) String language,
-											   @RequestParam(required = false) Integer year,
-											   @RequestParam(required = false) String category){
+											   @RequestParam(required = false) Integer year){
 		Specification<Book> spec = Specification.where(new BookWithTitle(title))
 			.and(new BookWithIsbn(isbn))
 			.and(new BookWithPages(pages))
 			.and(new BookWithPublished(published))
 			.and(new BookWithDescription(description))
 			.and(new BookWithLanguage(language))
-			.and(new BookWithYear(year))
-			.and(new BookWithCategory(category));
+			.and(new BookWithYear(year));
 		List<Book> books = bookService.findAllWithfilters(spec);
 		if(books.isEmpty()){
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
